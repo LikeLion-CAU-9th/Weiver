697b3de93fc88a33c1a9f81278ae72689e6192c0
Leo Bang <bluke8489@gmail.com>
2021-06-28 21:39:57 +0900

    add: class CommunityComment, on models.py

--- Code_Hunter/community/models.py
+++ Code_Hunter/community/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 from django.db.models.fields import CharField
 from django.utils import timezone
 import datetime
@@ -23,4 +24,9 @@ class Community(models.Model):
         return self.body[:100]
 
 
-
+class CommunityComment(models.Model):
+    id = models.BigAutoField(help_text="Comment ID", primary_key=True)
+    author = models.CharField(max_length=20, verbose_name="작성자", default= "익명")
+    content = models.TextField()
+    pub_datetime = models.DateTimeField()
+    origin_post = models.ForeignKey(Community, on_delete=CASCADE)
