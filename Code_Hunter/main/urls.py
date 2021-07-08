5b3cdeec4ad9ac1db130de9e36015b0817b96027
Leo Bang <bluke8489@gmail.com>
2021-07-08 11:45:08 +0900

    update login page

--- Code_Hunter/main/urls.py
+++ Code_Hunter/main/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name="index"),
+    path('account_base', account_base, name = "aaaaa"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
