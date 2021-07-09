2212e4900edebaefaac6ddac62ec778fb92fe8eb
Leo Bang <bluke8489@gmail.com>
2021-07-10 00:06:35 +0900

    community pagination

--- Code_Hunter/community/views.py
+++ Code_Hunter/community/views.py
@@ -9,7 +9,7 @@ import datetime
 
 def community(request):
     communities = Community.objects.all().order_by('-notice_or_not', '-pub_date')
-    paginator = Paginator(communities, 2)
+    paginator = Paginator(communities, 16)
     page = request.GET.get('page')
     if page == None: 
         page = 1
@@ -17,7 +17,7 @@ def community(request):
     communities = paginator.get_page(page)
     # print("page is ", page)
     date_now = datetime.datetime.now().date()
-    return render(request,'community.html', {'communities': communities, 'date_now':date_now, 'p_range' : paginate_range, 'start_page': start_page, 'end_page': end_page, 'current_page': page})
+    return render(request,'community.html', {'communities': communities, 'date_now':date_now, 'p_range' : paginate_range, 'start_page': start_page, 'end_page': end_page,})
 
 def make_pagination_range(current_page, paginator):
     # pagination 10단위로 만들어주는 funtion
