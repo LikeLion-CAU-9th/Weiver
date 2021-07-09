f562c60b94fbdd5257f5d16f407efe3d291c7b0b
Leo Bang <bluke8489@gmail.com>
2021-07-09 22:27:07 +0900

    add pagination function

--- Code_Hunter/community/views.py
+++ Code_Hunter/community/views.py
@@ -9,11 +9,28 @@ import datetime
 
 def community(request):
     communities = Community.objects.all().order_by('-notice_or_not', '-pub_date')
-    paginator = Paginator(communities, 5)
+    paginator = Paginator(communities, 1)
     page = request.GET.get('page')
+    if page == None: 
+        page = 1
+    paginate_range, start_page, end_page = make_pagination_range(page, paginator)
     communities = paginator.get_page(page)
+    # print("page is ", page)
     date_now = datetime.datetime.now().date()
-    return render(request,'community.html', {'communities': communities, 'date_now':date_now})
+    return render(request,'community.html', {'communities': communities, 'date_now':date_now, 'p_range' : paginate_range, 'start_page': start_page, 'end_page': end_page})
+
+def make_pagination_range(current_page, paginator):
+    # pagination 10단위로 만들어주는 funtion
+    if int(current_page) % 10 == 0:
+        page_decimal = int(current_page) // 10 - 1
+    elif int(current_page) % 10 != 0:
+        page_decimal = int(current_page) // 10
+    start_page = page_decimal * 10 + 1
+    end_page = page_decimal * 10 + 10
+    if end_page > paginator.num_pages:
+        return range(start_page, paginator.num_pages + 1), start_page, end_page
+    else:
+        return range(start_page, end_page + 1), start_page, end_page
 
 def create(request):
     new_post = Community()
