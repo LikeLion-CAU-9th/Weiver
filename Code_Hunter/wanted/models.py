986e03aaecdc591ccae3bdb84c55219bdb3a6993
Tarakyu <worrasong@gmail.com>
2021-07-14 22:27:37 +0900

    add account model to quest

--- Code_Hunter/wanted/models.py
+++ Code_Hunter/wanted/models.py
@@ -4,24 +4,19 @@ from django.db.models.deletion import CASCADE
 from django.utils import timezone
 from taggit.managers import TaggableManager
 import datetime
+from accounts.models import CustomUser
 
 class Quest(models.Model):
     SHOW_TYPES = [
         ('PUBLIC', 'PUBLIC'),
         ('PRIVATE', 'PRIVATE'),
     ]
-    TAGS = [
-        ('PYTHON', 'PYTHON'),
-        ('HTML', 'HTML'),
-        ('CSS', 'CSS'),
-        ('JS', 'JS'),
-    ]
     STATUS = [
         ('UNSOLVED', 'UNSOLVED'),
         ('SOLVING', 'SOLVING'),
         ('SOLVED', 'SOLVED'),
     ]
-    author = models.CharField(max_length=8, verbose_name='닉네임', default='코딩뉴비')
+    author = models.ForeignKey(CustomUser, verbose_name = "작성자", on_delete = models.CASCADE)
     title = models.CharField(max_length=50, verbose_name='제목')
     date = models.DateTimeField(auto_now_add=True)
     show = models.CharField(max_length = 10, choices=SHOW_TYPES, default='PUBLIC', help_text="비공개 여부")
@@ -39,14 +34,14 @@ class Quest(models.Model):
 
 class QuestComment(models.Model):
     quest = models.ForeignKey(Quest, on_delete=models.CASCADE, related_name='comments')
-    author =  models.CharField(max_length=8)
+    author =  models.ForeignKey(CustomUser, verbose_name = "작성자", on_delete = models.CASCADE)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
 class Review(models.Model):
     quest = models.ForeignKey(Quest, on_delete=models.CASCADE, related_name="review")
     title = models.CharField(max_length=50, verbose_name='제목')
-    author = models.CharField(max_length=8)
+    author = models.ForeignKey(CustomUser, verbose_name = "작성자", on_delete = models.CASCADE)
     body = models.TextField(verbose_name='내용')
     code = models.TextField(default='')
     file = models.FileField(null=True, verbose_name='첨부파일', blank=True)
